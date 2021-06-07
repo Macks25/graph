23089d760356518f856450a73929f982e9a71fd7
Max <zeller.maximilian@gmx.net>
2021-06-07 17:43:01 +0200

    intial

---
+++ graph/src/main/java/com/zeller/graph/NewClass.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zeller.graph;
+
+/**
+ *
+ * @author zelle
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        Graph greundesGraph = new Graph();
+        
+        Vertex tom = new Vertex("Tom");
+        greundesGraph.addVertex(tom);
+        Vertex otto = new Vertex("otto");
+        greundesGraph.addVertex(otto);
+        Vertex franz = new Vertex("franz");
+        greundesGraph.addVertex(franz);
+        Vertex paul = new Vertex("paul");
+        greundesGraph.addVertex(paul);
+        
+        Edge e1 = new Edge(paul, otto, 1);
+        Edge e2 = new Edge(tom, franz, 1);
+        Edge e3 = new Edge(franz, paul, 1);
+        
+        greundesGraph.addEdge(e1);
+        greundesGraph.addEdge(e2);
+        greundesGraph.addEdge(e3);
+        
+        
+    }
+}
